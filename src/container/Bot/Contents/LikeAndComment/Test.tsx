bb6993f404d5bb32d3ff5daa9f65512c85ff8839
dinhbinhnguyen0105 <dinhbinhnguyen0105@gmail.com>
2025-03-13 10:43:11 +0700

    from macmini

--- src/container/Bot/Contents/LikeAndComment/Test.tsx
+++ src/container/Bot/Contents/LikeAndComment/Test.tsx
@@ -1,9 +1,16 @@
 import InputField, { InputFieldProps } from "~/components/InputField/InputField";
+
 const Test: React.FC = () => {
 
     return (
         <div className="container">
-
+            <InputField
+                type="checkbox"
+                checked={false}
+                label="test"
+                className="test"
+                onChange={ }
+            />
         </div>
     );
 };
